5e55f33d33d8871dc9208895c104941a3e637b67
paulomachava <machava.pauloxavier@gmail.com>
2024-06-24 18:15:55 +0200

    create and run server

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignOut from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
   <BrowserRouter>
+  <Header/>
      <Routes>
       <Route path='/' element={<Home/>}/> 
       <Route path='/sign-in' element={<SignIn/>}/> 
